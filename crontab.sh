1aed85c7958ca9098eb79e72c8c044dafb97bea8
BharatRKadam <158236006+BharatRKadam@users.noreply.github.com>
2024-02-10 22:54:29 +0530

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,7 @@
 if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   echo "Build was successful, running additional steps..."
   # Add additional commands here
-  touch cron666
+  touch cron888
 else
   echo "Build failed, skipping additional steps."
 fi
